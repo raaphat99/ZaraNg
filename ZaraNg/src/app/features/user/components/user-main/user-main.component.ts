fb26958163dedd503f09246a484b8a0eef911836
Rawan <Rawanwaly27@gmail.com>
2024-10-08 22:39:31 +0300

    Complete Notification Page

--- ZaraNg/src/app/features/user/components/user-main/user-main.component.ts
+++ ZaraNg/src/app/features/user/components/user-main/user-main.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NotificationComponent } from "../notification/notification.component";
 
 @Component({
   selector: 'app-user-main',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NotificationComponent],
   templateUrl: './user-main.component.html',
   styleUrl: './user-main.component.css'
 })
 export class UserMainComponent {
-
+  isFindNotificationsOpen = false;
+  
 }
