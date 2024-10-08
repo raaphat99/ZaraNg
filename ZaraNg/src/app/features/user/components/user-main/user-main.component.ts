e9776a9630c8050c34f50dcc2d2b0483bc7ab4b3
Rawan <Rawanwaly27@gmail.com>
2024-10-08 18:33:06 +0300

    User Main Page And Some Changes in Header Responsive

--- ZaraNg/src/app/features/user/components/user-main/user-main.component.ts
+++ ZaraNg/src/app/features/user/components/user-main/user-main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-main',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './user-main.component.html',
   styleUrl: './user-main.component.css'
 })
--- ZaraNg/src/app/features/user/user-routing.module.ts
+++ ZaraNg/src/app/features/user/user-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserMainComponent } from './components/user-main/user-main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserMainComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
