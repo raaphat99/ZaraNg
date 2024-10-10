8603b2843029eb456e6c3d52094b7ca0a23a9c26
AhmedAbdelbadeaa <a.m.abdelbadea@gmail.com>
2024-10-10 13:49:50 +0300

    shopping bag / wishlist finished (FrontEnd)

--- ZaraNg/src/app/app.component.ts
+++ ZaraNg/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { CartComponent } from "./features/shopping/cart/cart.component";
+import { WishlistComponent } from "./features/shopping/wishlist/wishlist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CartComponent, WishlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
