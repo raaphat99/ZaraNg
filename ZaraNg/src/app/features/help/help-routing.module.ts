fed1a5bf1ecc8ffd10aec90b71ccd0098f9e5afd
Ahmed Raaphat <ahmed1999raafat@gmail.com>
2024-10-12 00:50:54 +0300

    Fix some issues in the Help page design

--- ZaraNg/src/app/features/help/help-routing.module.ts
+++ ZaraNg/src/app/features/help/help-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HelpComponent } from './components/help/help.component';
+import { HelpCenterComponent } from './components/help-center/help-center.component';
 
 const routes: Routes = [
-  {path: '', component: HelpComponent}
+  {path: '', component: HelpCenterComponent},
 ];
 
 @NgModule({
