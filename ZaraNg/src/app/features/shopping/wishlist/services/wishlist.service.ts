c1f72630d76e63da33b4261d2abe4f72f38f0a18
AhmedAbdelbadeaa <a.m.abdelbadea@gmail.com>
2024-10-16 10:18:00 +0300

    finished cart and wishlist

--- ZaraNg/src/app/features/shopping/wishlist/services/wishlist.service.ts
+++ ZaraNg/src/app/features/shopping/wishlist/services/wishlist.service.ts
@@ -3,69 +3,57 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
-export interface CartItemDTO {
+export interface WishListItemDTO {
   id: number;
-  productVariantId: number;
-  quantity: number;
-  color: string;
-  size: string;
-  title: string;
-  imageUrl: string;
+  name: string;
   price: number;
+  imageUrl: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
-export class CartService {
-  private apiUrl = 'http://localhost:5250/api/Cart'; 
+export class WishlistService {
+  private apiUrl = 'http://localhost:5250/api/WishList';
 
   constructor(private http: HttpClient) { }
 
-  // Method to get all cart items for a user
-  getCartItems(): Observable<CartItemDTO[]> {
+  // Method to get all wishlist items for a user
+  getAllItems(): Observable<WishListItemDTO[]> {
     const token = localStorage.getItem('token');
     const headers = { 'Authorization': `Bearer ${token}` };
-    return this.http.get<CartItemDTO[]>(`${this.apiUrl}`, { headers }).pipe(
+    return this.http.get<WishListItemDTO[]>(`${this.apiUrl}`, { headers }).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Method to add a new item to the cart
-  addCartItem(productVariantId: number): Observable<any> {  
+  // Method to add a new item to the wishlist
+  addToWishlist(productId: number): Observable<any> {
     const token = localStorage.getItem('token');
     const headers = { 'Authorization': `Bearer ${token}` };
-    return this.http.post<any>(`${this.apiUrl}/${productVariantId}`, {headers},{observe:'response'}).pipe(
-      tap((response) => {   
-        console.log(response);
-        console.log('Item added to cart successfully');  
-
-    })
-    ); 
-  }
-
-  // Method to remove an item from the cart
-  removeOrDecreaseCartItem(cartItemId: number): Observable<string> {
-    return this.http.delete(`${this.apiUrl}/${cartItemId}`, {
-      responseType: 'text'
-    }).pipe(
-      catchError(this.handleError)
-    );
-  }
-
-  moveToWishlist(cartItemId: number): Observable<any> {
-    const token = localStorage.getItem('token');
-    const headers = { 'Authorization': `Bearer ${token}` };
-    return this.http.post<any>(`${this.apiUrl}/move-to-wishlist/${cartItemId}`, {headers}).pipe(
+    return this.http.post<any>(`${this.apiUrl}/${productId}`, {headers},{observe : 'response'}).pipe(
       tap((response) => {
-        console.log('Item moved to wishlist successfully');
+        console.log(response);
+        console.log('Item added to wishlist successfully');
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  // Method to move an item from wishlist to cart
+  moveToCart(wishlistItemId: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = { 'Authorization': `Bearer ${token}` };
+    return this.http.post<any>(`${this.apiUrl}/move-to-cart/${wishlistItemId}`,{headers}).pipe(
+      tap((response) => {
+        console.log('Item moved to cart successfully');
         console.log(response);
       }),
       catchError(this.handleError)
     );
   }
-  
 
+  // Method to handle errors
   private handleError(error: HttpErrorResponse): Observable<never> {
     let errorMessage = 'An unknown error occurred!';
     if (error.error instanceof ErrorEvent) {
@@ -83,4 +71,5 @@ export class CartService {
     console.error(errorMessage);
     return throwError(() => new Error(errorMessage));
   }
+
 }
